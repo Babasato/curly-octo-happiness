8671b74dc8da860e10d3d2626c0b6e65e9423937
Babasato <114899243+Babasato@users.noreply.github.com>
2025-12-21 09:29:53 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,6 +3,26 @@ const nextConfig = {
   experimental: {
     serverComponentsExternalPackages: ['resend'],
   },
+  async redirects() {
+    return [
+      {
+        source: '/free-resources.html',
+        destination: '/resources',
+        permanent: true,
+      },
+      {
+        source: '/free-resources',
+        destination: '/resources',
+        permanent: true,
+      },
+      {
+        // This catches all the old PDF links in the /downloads/ folder
+        source: '/downloads/:path*',
+        destination: '/resources',
+        permanent: true,
+      },
+    ]
+  },
 };
 
 module.exports = nextConfig;
