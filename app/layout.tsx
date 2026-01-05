4ded77036958cc3ea824b50094ca5bbc860900fb
Babasato <114899243+Babasato@users.noreply.github.com>
2026-01-05 21:11:01 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,7 @@ import { Metadata } from 'next';
 export const metadata: Metadata = {
   metadataBase: new URL('https://homeschoolmath.site'),
   alternates: {
-    canonical: '/',
+    canonical: 'https://homeschoolmath.site',
   },
 };
 
