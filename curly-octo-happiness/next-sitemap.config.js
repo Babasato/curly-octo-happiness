ed4b067a9cba28521f45e3ae47e949cc77ca7c23
Babasato <114899243+Babasato@users.noreply.github.com>
2026-06-03 08:57:45 +0300

    Remove stale nested duplicate folder

--- curly-octo-happiness/next-sitemap.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('next-sitemap').IConfig} */
-module.exports = {
-  siteUrl: 'https://homeschoolmath.site',
-  generateRobotsTxt: true,
-  changefreq: 'weekly',
-  priority: 0.7,
-  sitemapSize: 5000,
-}
